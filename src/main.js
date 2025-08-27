c1ba8e751522ca901beb73fe1ab83855715e30ed
AlexRg32 <alexrg32@outlook.es>
2025-08-27 18:29:52 +0200

    arreglado bug carrousel

--- src/main.js
+++ src/main.js
@@ -5,32 +5,60 @@ const btnAnterior = document.querySelector('.carousel__btn--prev');
 let indice = 0;
 
 function moverCarrusel() {
-
-  if(window.innerWidth < 768){
-    contenedorCarrusel.style.transform = 'translateX(' + (indice * -100) + '%)';
-  }else{
-    contenedorCarrusel.style.transform = `translateX(-${indice * (100/3)}%)`;
+  if (window.innerWidth < 768) {
+    contenedorCarrusel.style.transform = `translateX(-${indice * 100}%)`;
+  } else {
+    contenedorCarrusel.style.transform = `translateX(-${indice * (100 / 3)}%)`;
   }
   console.log(indice);
 }
 
-moverCarrusel();
 
-btnAnterior.addEventListener('click', () => {
-  if (indice === 0) {
-    indice = 3;
+function siguiente() {
+  if (window.innerWidth < 768) {
+   
+    if (indice === 5) {
+      indice = 0;
+    } else {
+      indice++;
+    }
   } else {
-    indice--;
+  
+    if (indice === 3) {
+      indice = 0;
+    } else {
+      indice++;
+    }
   }
-
   moverCarrusel();
-})
+}
 
-btnSiguiente.addEventListener('click', () => {
-  if (indice === 3) {
-    indice = 0;
+
+function anterior() {
+  if (window.innerWidth < 768) {
+   
+    if (indice === 0) {
+      indice = 5;
+    } else {
+      indice--;
+    }
   } else {
-    indice++;
+    
+    if (indice === 0) {
+      indice = 3;
+    } else {
+      indice--;
+    }
   }
   moverCarrusel();
-})
+}
+
+
+btnSiguiente.addEventListener('click', siguiente);
+btnAnterior.addEventListener('click', anterior);
+
+
+window.addEventListener('resize', () => {
+  indice = 0;
+  moverCarrusel();
+});
